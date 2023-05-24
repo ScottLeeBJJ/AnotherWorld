c07500a03d78a0aee0869828dcf12e9b96064bfd
Scott Lee <scottlee5455@outlook.com>
2023-05-24 16:32:44 -0500

    Setting up movement controls for the Player
    Using WASD for movement controls, possibly gamepad controls as well.

---
+++ AnotherWorld/Assets/playerBaseState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBaseState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize player idle animation on game start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // play BGM
+        // player walk, run, jump, strafe, running jump, and so on....
+    }
+}
--- AnotherWorld/Assets/playerMovement.cs
+++ AnotherWorld/Assets/playerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public float movementDirection;
+    public float walkSpeed = 2f;
+    public float runSpeed = 5f;
+    public float sprintSpeed = 10f;
+    public float jumpHeight = 6f;
+    private bool isMoving = false;
+    private bool isSprinting = false;
+
+    private Rigidbody rigidBody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            rigidBody.velocity += transform.forward * 
+        }
     }
 }
