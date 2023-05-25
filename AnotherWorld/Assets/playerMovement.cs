f238b6d1218756ad5755cbe972da16e12bef024e
Scott Lee <scottlee5455@outlook.com>
2023-05-24 20:39:57 -0500

    playerController added, thanks Steve, WASD set.

--- AnotherWorld/Assets/playerMovement.cs
+++ AnotherWorld/Assets/playerMovement.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class playerMovement:MonoBehaviour
 {
-    public float movementDirection;
-    public float walkSpeed = 2f;
-    public float runSpeed = 5f;
-    public float sprintSpeed = 10f;
-    public float jumpHeight = 6f;
-    private bool isMoving = false;
-    private bool isSprinting = false;
-
-    private Rigidbody rigidBody;
-
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2f;
+    private float jumpHeight = 10f;
+    private float gravityValue = -10;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
         {
-            rigidBody.velocity += transform.forward * 
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
         }
     }
 }
